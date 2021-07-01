c5abced2e9c742b72139f6125828ac702979825f
sonic_EA <sonic@delmic.com>
2021-07-01 16:26:59 +0200

    some adjustments: descan offset seems to be calculated fine now, threading.Event seems to work

--- src/odemis/acq/acquire_fastem_stage_LUT.py
+++ src/odemis/acq/acquire_fastem_stage_LUT.py
@@ -56,9 +56,13 @@ def acquire_MegaField(mppc, dataflow, descanner, multibeam, stage, ccd, beamshif
     #setting of the descanner
     # offset_x = 0.09876788979391249
     # offset_y = 0.16016329116497904
+    # good offset positions
+    good_offset_x = 0.1586
+    good_offset_y = 0.2166
     # new values after realignment:
     offset_x = 0.20
     offset_y = 0.28
+    print("inital descan offset x: {}; inital descan offset y: {}".format(offset_x, offset_y))
 
     descanner.scanOffset.value = (offset_x, offset_y)
     descanner.scanGain.value = (offset_x + 0.0082, offset_y - 0.0082)
@@ -72,35 +76,33 @@ def acquire_MegaField(mppc, dataflow, descanner, multibeam, stage, ccd, beamshif
     multibeam.dwellTime.value = 0.4e-6
 
     # TODO replace with get call; disadvantage it will be labelled as (0,0), which is not handy for debugging
-    dataflow.subscribe(on_field_image)
+    dataflow.subscribe(on_field_image)  # no event set yet; only when receiving data
     # clear event that is set in the callback
     image_received.clear()
-    dataflow.next((1001, 1001))
-    image_received.wait(dwell_time * mppc.cellCompleteResolution.value[0] * mppc.cellCompleteResolution.value[1] + 1)
+    dataflow.next((1001, 1001))  # generate data,
+    logging.debug("requested calibration image acquired via ASM")
+    # image_received.wait(2e-6 * mppc.cellCompleteResolution.value[0] * mppc.cellCompleteResolution.value[1] + 1)
+    if not image_received.wait(multibeam.dwellTime.value * mppc.cellCompleteResolution.value[0] * mppc.cellCompleteResolution.value[1] + 1):
+        # wait until the current status is returned; if status not True (no data received by callback function and
+        # thus event not set to true, but False (timeout) -> raise error
+        raise TimeoutError("Calibration timed out")
+    # blocking; callback function receive data; event is set to True; wait until flag is true
+    logging.debug("received calibration image from ASM")
     dataflow.unsubscribe(on_field_image)
 
-    # FIXME dataflow.next() is not blocking. scan_field is supposed to be blocking
-    # FIXME To be sure the settings have been applied to
-    #  the HW, we added the next call, as then it seems to only move the beam to the correct offset value.
-    #  Check why settings not already set on HW in start/subscribe.
-
-    # It looks like with the new implementation the offset is still not uploaded when getting the DC image, which is
-    # used to calculate the correct descan offset (which is visible by dark cell images in the field image and/or
-    # ghosting of structures
-    # time.sleep(30)
-
     ccd_image = ccd.data.get(asap=False)
+    logging.debug("received diagnostic camera image")
     spot_coordinates, *_ = FindGridSpots(ccd_image, (8, 8))
     spot_coordinates[:, 1] = ccd_image.shape[1] - spot_coordinates[:, 1]
     shift_descan = numpy.mean(spot_coordinates, axis=0) - mean_spot
+    print("correction for descan offset: {}".format(shift_descan * (3.45 / 10)))
     offset_x = offset_x+shift_descan[0] * 0.000196022
     offset_y = offset_y+shift_descan[1] * 0.000196022
-    print("shift descan offset: {}".format(shift_descan*(3.45/10)))
     descanner.scanOffset.value = (offset_x, offset_y)
     descanner.scanGain.value = (offset_x + 0.0082/1, offset_y - 0.0082/1)
 
-    print("descan offset x: {}; descan offset y: {}".format(offset_x, offset_y))
-    # time.sleep(3)
+    print("expected descan offset x: {}; calibrated descan offset y: {}".format(good_offset_x, good_offset_y))
+    print("calibrated descan offset x: {}; calibrated descan offset y: {}".format(offset_x, offset_y))
 
     #final settings for the acquisition
     multibeam.dwellTime.value = dwell_time
@@ -113,7 +115,7 @@ def acquire_MegaField(mppc, dataflow, descanner, multibeam, stage, ccd, beamshif
     mppc.cellTranslation.value = tuple(tuple((0, 0) for i in range(0, mppc.shape[0])) for i in range(0, mppc.shape[1]))
     overlap = 0.075  # the fractional overlap
 
-    if std_dark_gain == 'True':
+    if std_dark_gain:
         mppc.cellDarkOffset.value = ((32609, 32512, 32632, 32707, 32833, 32590, 32129, 32915),(32775, 32782, 32607, 32731, 32974, 32770, 32980, 32787),(32480, 32403, 32931, 32905, 32734, 33041, 32779, 32821),(33012, 32958, 32473, 32462, 32235, 32570, 32558, 32735),(32871, 32687, 32583, 32591, 32898, 32750, 32748, 32875),(32497, 32963, 32294, 32607, 32681, 32585, 32799, 32562),(32357, 32625, 32650, 32630, 33080, 32741, 32606, 32766),(32687, 32809, 32393, 32497, 32740, 32279, 32634, 32579))
         mppc.cellDigitalGain.value = ((1.0310130234815644, 0.98911785399073, 1.0181627423471624, 0.8577040961758531, 0.8880908034347579, 0.9890579976899265, 0.9383996622711986, 1.1113391502317609),(0.8593799077749432, 0.8986987222165352, 0.9363027653136717, 0.822184009271164, 0.8479924018374657, 0.9021588056776584, 0.837452960039753, 1.058894453119832),(0.8465728724395954, 0.7903281330555539, 0.8599569977952789, 0.8457909791302854, 0.8256918838727555, 0.8533798498272129, 0.8002763945317064, 1.0731022584055032),(0.8621492151552735, 0.7531764833868415, 0.7352312175442495, 0.7401340718521829, 0.7597036584238763, 0.844865700403647, 0.7770241838127158, 0.9983141992621981),(0.8665074854440534, 0.8098175493432067, 0.7311517895733345, 0.6948023507297094, 0.7318181050458212, 0.8681639239369103, 0.7755633808103727, 0.8923224747397783),(0.8335233462552188, 0.7881700089377985, 0.749662487713841, 0.7158504604573277, 0.701682390230767, 0.7977500467587251, 0.7914494398257048, 0.9763284195350683),(0.8521377276528598, 0.7643034555179854, 0.7983025624619302, 0.8379968529320432, 0.7114937102240616, 0.7570616654458873, 0.8074662374392703, 1.0288508064914936),(0.9513008682286824, 0.8964435843490056, 0.8529914655577808, 0.995611934322976, 0.8125292911222118, 0.8228977944874873, 0.9327903752318961, 1.040315455335232))
     # set_overscan_parameters_from_csv(mppc, "/home/fast-em/Desktop/over_scan_params/overscan_parameters.csv") #comment for FIELD CORRECIONS
@@ -123,12 +125,6 @@ def acquire_MegaField(mppc, dataflow, descanner, multibeam, stage, ccd, beamshif
 
     # mppc.cellDarkOffset.value = tuple(tuple(0 for i in range(0, mppc.shape[0])) for i in range(0, mppc.shape[1]))
     # mppc.cellDigitalGain.value = tuple(tuple(1 for i in range(0, mppc.shape[0])) for i in range(0, mppc.shape[1]))
-
-    mppc.dataContent.value = "empty"
-    mppc.filename.value = time.strftime("testing_megafield_id-%Y-%m-%d-%H-%M-%S")
-    dataflow = mppc.data
-    dataflow.subscribe(on_field_image)
-    # time.sleep(3)
 
     #here lookup tables of the stage positions are created
     x_stage_pos = - numpy.array([numpy.linspace(0, ((field_images[0] - 1) * 3.195e-6 * 8 * (1 - overlap)), field_images[0])] * field_images[1])
@@ -147,42 +143,49 @@ def acquire_MegaField(mppc, dataflow, descanner, multibeam, stage, ccd, beamshif
 
     pos = stage.position.value
 
+    mppc.dataContent.value = "empty"
+    mppc.filename.value = time.strftime("testing_megafield_id-%Y-%m-%d-%H-%M-%S")
+    dataflow = mppc.data
+
+    dataflow.subscribe(on_field_image)
+
     for row in range(field_images[1]):  # y axis, move stage on y,
-
         for col in range(field_images[0]):  # x axis, move stage on x
-
-            print("col (x): {} row (y): {} stage: {}(this does not mean a lot)".format(col, row, stage.position.value))
+            print("acquire field image with col (x): {} and row (y): {} ".format(col, row))
 
             ####################################################################################
             # Correct for paramagnetic field of the stage using the beam shift.
-            # if col > 0 or row > 0:  # skip first image
-            #     ccd_image = ccd.data.get(asap=False)
-            #     spot_coordinates, *_ = FindGridSpots(ccd_image, (8, 8))
-            #     # Transfer to a coordinate system with the origin in the bottom left.
-            #     spot_coordinates[:, 1] = ccd_image.shape[1] - spot_coordinates[:, 1]
-            #     print("spot_coordinates position: {}".format(numpy.mean(spot_coordinates, axis=0)))
-            #     shift = numpy.mean(spot_coordinates, axis=0) - mean_spot
-            #     # Compensate for shift with dc beam shift
-            #     # convert shift from pixels to um
-            #     shift_um = shift * 3.45e-6 / 40  # pixelsize ueye cam and 40x magnification
-            #     print("beam shift required due to stage magnetic field (um): {}".format(shift_um))
-            #     cur_beam_shift_pos = numpy.array(beamshift.shift.value)
-            #     beamshift.shift.value = (shift_um + cur_beam_shift_pos)
-            #     print("current beam shift um: {}".format(beamshift.shift.value))
+            if col > 0 or row > 0:  # skip the first image as not needed for it
+                ccd_image = ccd.data.get(asap=False)
+                spot_coordinates, *_ = FindGridSpots(ccd_image, (8, 8))
+                # Transfer to a coordinate system with the origin in the bottom left.
+                spot_coordinates[:, 1] = ccd_image.shape[1] - spot_coordinates[:, 1]
+                print("spot_coordinates position: {}".format(numpy.mean(spot_coordinates, axis=0)))
+                shift = numpy.mean(spot_coordinates, axis=0) - mean_spot
+                # Compensate for shift with dc beam shift
+                # convert shift from pixels to um
+                shift_um = shift * 3.45e-6 / 40  # pixelsize ueye cam and 40x magnification
+                print("beam shift adjustment required due to stage magnetic field (um): {}".format(shift_um))
+                cur_beam_shift_pos = numpy.array(beamshift.shift.value)
+                print("current beam shift um: {}".format(beamshift.shift.value))
+                beamshift.shift.value = (shift_um + cur_beam_shift_pos)
+                print("new beam shift um: {}".format(beamshift.shift.value))
 
             ##############################################################################
-            ##stage move with mm correction ############################################################################
-            # # TODO move into separate funciton call
-            # # stage pos correction due to inaccuracy in stage pos
-            # # move the stage by absolute position based on first field image position -> calculate next stage position
-            # # (to avoid accumulation of relative stage movement errors)
-            # # Move stage one field in the positive x ddeirection of the scanner.
-            # # Sample should move on TSF screen to the right if we apply a positive x stage move.
-            # # (0,0) is top left corner -> move sample to the left, which is negative x stage move.
+            #stage move with mm correction ############################################################################
+            # # TODO move into separate function call
+            # stage pos correction due to inaccuracy in stage pos
+            # move the stage by absolute position based on first field image position -> calculate next stage position
+            # (to avoid accumulation of relative stage movement errors)
+            # Move stage one field in the positive x ddeirection of the scanner.
+            # Sample should move on TSF screen to the right if we apply a positive x stage move.
+            # (0,0) is top left corner -> move sample to the left, which is negative x stage move.
             if col > 0 or row > 0:
+                logging.debug("moving the stage")
                 stage.moveAbs({"x": x_stage_pos[row, col]}).result()  # in meter!
                 stage.moveAbs({"y": y_stage_pos[row, col]}).result()  # in meter!
-                # time.sleep(0.5)
+                time.sleep(0.4)  # TODO needed for settling of stage?
+
             #     # read new MM position
             #     stage_pos_actual = (0, 0)
 
@@ -222,25 +225,30 @@ def acquire_MegaField(mppc, dataflow, descanner, multibeam, stage, ccd, beamshif
                 # ##############################################################################
                 # time.sleep(0.5)
 
+            print("acquire field image with col (x): {} and row (y): {} at stage position: {}"
+                  .format(col, row, stage.position.value))
+
             # clear event that is set in the callback
             image_received.clear()
-            # acquire field
+            # request next field
             dataflow.next((col, row))   # acquire the field image (y, x)
-            # TODO fix in driver the waiting time for the first image that is overwritten, the callback does not take this into account here; add some extra time (30s)
-            image_received.wait(dwell_time * mppc.cellCompleteResolution.value[0] * mppc.cellCompleteResolution.value[1] + 30)
+            if not image_received.wait(dwell_time * mppc.cellCompleteResolution.value[0] * mppc.cellCompleteResolution.value[1] + 30):
+                # wait returns the current status; if status not True then no data received by callback function and
+                # event was not set to true, but False (timeout) -> raise error
+                raise TimeoutError("!!!!!timeout during megafield acquisition!!!!!!!!!!!!!!!!!!!!!")
+                # TODO fix in driver the waiting time for the first image that is overwritten, the callback does not take this into account here; add some extra time (30s)
+            # image_received.wait(dwell_time * mppc.cellCompleteResolution.value[0] * mppc.cellCompleteResolution.value[1] + 30)
             # #TODO investigate order!! with testcases in wrapper order not checked with filename pattern
             # TODO implement proper future for image acquisition (will be done in acq manager)
 
         ##############################################################################
-        # time.sleep(0.5)
-
     dataflow.unsubscribe(on_field_image)
 
     # Move stage back to starting position of the stage
     stage.moveAbs(pos).result()
 
 ##############################################################################
-# stage position correction
+# plan for stage position correction
 
 # read out position stage and position metrology module
 # map MM position to stage position: calculate the offset between the 2 coordinate systems
@@ -251,6 +259,7 @@ def acquire_MegaField(mppc, dataflow, descanner, multibeam, stage, ccd, beamshif
 # calc difference from theoretical stage pos and MM (actual stage) pos taking the offset into account
 # correct with beam shift (first read, then set as also absolute pos)
 ##############################################################################
+
 
 image_received = threading.Event()
 
@@ -290,6 +299,7 @@ def main(args):
     scanner.blanker.value = False  # unblank the beam
     scanner.horizontalFoV.value = 2.2e-05  # corresponds to mag = 5000x in quad view
     scanner.multiBeamMode.value = True  # True selects multibeam mode
+    scanner.immersion.value = True  # put the microscope in immersion mode if not already
     scanner.external.value = True  # Put microscope in external mode
     # scanner.power.value = True  # Switch on beam
     # Provide coordinate transform for beam shift.
@@ -297,8 +307,8 @@ def main(args):
 
     time.sleep(2)  # wait a bit
 
-    field_images = (1, 2)   # (x, y) Note: x (horizontal) = col, y (vertical) = row
-    dwell_time = 10e-6
+    field_images = (1, 1)   # (x, y) Note: x (horizontal) = col, y (vertical) = row
+    dwell_time = 2e-6
     # debugging: (1,3): expect 3 images in y direction (rows), files 0_0_, 1_0_, 2_0_
     # debugging: (2,3): expect 3 images in y direction (rows), 2 images in x dir (columns) files 0_0_, 1_0_, 2_0_, 0_1_, 1_1_, 2_1_
 
