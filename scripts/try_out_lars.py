75a656b9005932d062f1059c452cf26e3a6442d2
Lars van Roemburg <vanroemburg@delmic.com>
2022-02-21 15:38:30 +0100

    normalization and histogram added

--- scripts/try_out_lars.py
+++ scripts/try_out_lars.py
@@ -8,7 +8,9 @@ data_after_milling = tiff.read_data("/home/victoria/Documents/Lars/data/1/FOV2_G
 
 p1 = 17
 p2 = p1 - 7  # there is a 7 slices difference in the z position
-blur = 15
+blur = 25
+threshold_outliers = 300
+threshold_mask = 0.25
 
 img_cp0 = np.array(data_before_milling[0][0][0][p1], dtype=int)  # data_before_milling[0][0][0][17]
 img_cp4 = np.array(data_after_milling[0][0][0][p2], dtype=int)  # data_after_milling[0][0][0][17]
@@ -20,27 +22,59 @@ print(np.max(img_cp0))
 print(np.max(img_cp4))
 print("\n")
 
-img_cp0[img_cp0 > 300] = 300
-img_cp4[img_cp4 > 300] = 300
-img_cp4 = gaussian_filter(img_cp4, sigma=blur)
-img_cp0 = gaussian_filter(img_cp0, sigma=blur)
-diff = img_cp0 - img_cp4
-diff[diff<55] = 0
+img_cp0[img_cp0 > threshold_outliers] = threshold_outliers
+img_cp4[img_cp4 > threshold_outliers] = threshold_outliers
+img_cp4_blurred = gaussian_filter(img_cp4, sigma=blur)
+img_cp0_blurred = gaussian_filter(img_cp0, sigma=blur)
+
+print(np.min(img_cp0_blurred))
+print(np.min(img_cp4_blurred))
+print(np.max(img_cp0_blurred))
+print(np.max(img_cp4_blurred))
+print("\n")
+
+# normalization to [0,1] interval
+base_lvl_cp0 = np.min(img_cp0_blurred)
+base_lvl_cp4 = np.min(img_cp4_blurred)
+img_cp0_blurred = (img_cp0_blurred - base_lvl_cp0) / (np.max(img_cp0_blurred) - base_lvl_cp0)
+img_cp4_blurred = (img_cp4_blurred - base_lvl_cp4) / (np.max(img_cp4_blurred) - base_lvl_cp4)
+
+diff = img_cp0_blurred - img_cp4_blurred
+mask = diff >= threshold_mask
+masked_img = img_cp4_blurred * mask
+# masked_img[mask] = masked_img[mask] - base_lvl_cp4
+# masked_img[masked_img < 0] = 0
+
 print(np.min(img_cp0))
 print(np.min(img_cp4))
+print(np.min(img_cp0_blurred))
+print(np.min(img_cp4_blurred))
 print(np.min(diff))
 print("\n")
 print(np.max(img_cp0))
 print(np.max(img_cp4))
+print(np.max(img_cp0_blurred))
+print(np.max(img_cp4_blurred))
 print(np.max(diff))
 
-fig, ax = plt.subplots(2, 2)
+
+# plt.title('histogram')
+# plt.xlabel("value")
+# plt.ylabel("frequency")
+# plt.hist(diff.flatten())
+# plt.show()
+
+
+fig, ax = plt.subplots(3, 2)
 ax[0, 0].imshow(img_cp0)
 ax[0, 0].set_title("before")
 ax[0, 1].imshow(img_cp4)
 ax[0, 1].set_title("after")
-ax[1, 0].imshow(diff)
-ax[1, 0].set_title("the difference")
+ax[1, 0].imshow(img_cp0_blurred)
+ax[1, 1].imshow(img_cp4_blurred)
+ax[2, 0].imshow(diff)
+ax[2, 0].set_title("the difference")
+ax[2, 1].imshow(masked_img)
 plt.show()
 
 '''
