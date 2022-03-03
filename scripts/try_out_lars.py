180e4b2104f0436b4d36b8bf1c75fb33689dd5fa
Lars van Roemburg <vanroemburg@delmic.com>
2022-03-03 11:45:05 +0100

    z-stack mean and max projection implemented, still needs to be combined with single images.

--- scripts/try_out_lars.py
+++ scripts/try_out_lars.py
@@ -2,24 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from odemis.dataio import tiff
 from scipy.ndimage.filters import gaussian_filter
-from scipy.ndimage import binary_erosion, binary_opening, binary_closing
+from scipy.ndimage import binary_erosion, binary_dilation, binary_opening, binary_closing
 from scipy.signal import fftconvolve
-from cv2 import HoughCircles, HOUGH_GRADIENT
+# from cv2 import HoughCircles, HOUGH_GRADIENT
 
 # import analyse_shifts
 
 # which_path = 4
-threshold_mask = 0.25
-threshold_end = 0.2
+threshold_mask = 0.3
+threshold_end = 0.25
 blur = 25
 cropping = True
+squaring = False
+mean_zstack = False  # if true the mean is taken from the z-stack, if false the max intensity projection is taken
+max_zstack = True - mean_zstack
 
-channel_before = [0, 0, 0, 0, 0, 0, 0, 1]
-channel_after = [0, 0, 0, 0, 0, 0, 0, 0]
-manual_thr_out_before = [300, 700, 3000, 300, 300, 300, 300, 200]  # make this percentage of max image intensity
-manual_thr_out_after = [300, 700, 3000, 300, 300, 300, 300, 8000]  # make this percentage of max image intensity
-z_value_before = [20, 23, 20, 25, 30, np.pi, 25, np.pi]  # np.pi means it is not a z-stack, only one image
-z_value_after = [13, 15, 20, 19, 30, np.pi, 19, 1]  # np.pi means it is not a z-stack, only one image
+channel_before = [0, 0, 0, 0, 0, 0, 0, 1, 0]
+channel_after = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+manual_thr_out_before = [300, 700, 3000, 300, 300, 300, 300, 200, 750]  # make this percentage of max image intensity
+manual_thr_out_after = [300, 700, 3000, 300, 300, 300, 300, 8000, 900]  # make this percentage of max image intensity
+z_value_before = [20, 23, 20, 25, 30, np.pi, 25, np.pi, 27]  # np.pi means it is not a z-stack, only one image
+z_value_after = [13, 15, 20, 19, 30, np.pi, 19, 1, 22]  # np.pi means it is not a z-stack, only one image
 
 data_paths_before = ["/home/victoria/Documents/Lars/data/1/FOV2_GFP_cp00.tif",
                      "/home/victoria/Documents/Lars/data/2/FOV1_checkpoint_00.tiff",
@@ -30,7 +33,8 @@ data_paths_before = ["/home/victoria/Documents/Lars/data/1/FOV2_GFP_cp00.tif",
                      "/EA010_8_FOV3_checkpoint_00_1-24.tif",
                      "/home/victoria/Documents/Lars/data/meteor1/FOV3_Meteor_stacks/EA010_8_FOV3_checkpoint_00_1.tiff",
                      "/home/victoria/Documents/Lars/data/XA Yeast-20220215T093027Z-001/XA "
-                     "Yeast/20200918_millingsite_start.ome.tiff"
+                     "Yeast/20200918_millingsite_start.ome.tiff",
+                     "/home/victoria/Documents/Lars/data/Meteor_data_for_Lars/Mammalian_cells/FOV7_cp00_GFP.tif"
                      ]
 
 data_paths_after = ["/home/victoria/Documents/Lars/data/1/FOV2_GFP_cp04.tif",
@@ -42,48 +46,76 @@ data_paths_after = ["/home/victoria/Documents/Lars/data/1/FOV2_GFP_cp04.tif",
                     "/EA010_8_FOV3_final-19.tif",
                     "/home/victoria/Documents/Lars/data/meteor1/FOV3_Meteor_stacks/EA010_8_FOV3_final.tiff",
                     "/home/victoria/Documents/Lars/data/XA Yeast-20220215T093027Z-001/XA "
-                    "Yeast/20200918-zstack_400nm.ome.tiff"
+                    "Yeast/20200918-zstack_400nm.ome.tiff",
+                    "/home/victoria/Documents/Lars/data/Meteor_data_for_Lars/Mammalian_cells/FOV7_after_GFP.tif"
                     ]
-for nnn in np.arange(0, 8, 1, dtype=int):
+for nnn in np.arange(5, 9, 1, dtype=int):
     print("dataset nr. {}".format(nnn+1))
+
+    # convert the image to a numpy array and set a threshold for outliers in the image / z-stack
     data_before_milling = tiff.read_data(data_paths_before[nnn])
-    data_after_milling = tiff.read_data(data_paths_after[nnn])
-
     meta_before = data_before_milling[channel_before[nnn]].metadata
-    meta_after = data_after_milling[channel_after[nnn]].metadata
 
-    print("Pixel sizes are the same: {}".format(meta_before["Pixel size"][0] == meta_after["Pixel size"][0]))
-
-    # convert the image to a numpy array
     if z_value_before[nnn] != np.pi:
-        img_before = np.array(data_before_milling[channel_before[nnn]][0][0][z_value_before[nnn]],
-                              dtype=int)
+        img_before = np.array(data_before_milling[channel_before[nnn]][0][0], dtype=int)
+        del data_before_milling
+
+        histo, bins = np.histogram(img_before, bins=2000)
+        histo = np.cumsum(histo)
+        histo = histo / histo[-1]
+        plc = np.min(np.where(histo > 0.99))
+        thr_out_before = bins[plc] / 2 + bins[plc + 1] / 2
+        img_before[img_before > thr_out_before] = thr_out_before
+
+        if mean_zstack:
+            img_before = np.mean(img_before, axis=0)
+        else:
+            img_before = np.max(img_before, axis=0)
+
     else:
         img_before = np.array(data_before_milling[channel_before[nnn]], dtype=int)
+        del data_before_milling
+
+        histo, bins = np.histogram(img_before, bins=2000)
+        histo = np.cumsum(histo)
+        histo = histo / histo[-1]
+        plc = np.min(np.where(histo > 0.99))
+        thr_out_before = bins[plc] / 2 + bins[plc + 1] / 2
+        img_before[img_before > thr_out_before] = thr_out_before
+
+    del histo, bins
+
+    data_after_milling = tiff.read_data(data_paths_after[nnn])
+    meta_after = data_after_milling[channel_after[nnn]].metadata
 
     if z_value_after[nnn] != np.pi:
-        img_after = np.array(data_after_milling[channel_after[nnn]][0][0][z_value_after[nnn]],
-                             dtype=int)
+        img_after = np.array(data_after_milling[channel_after[nnn]][0][0], dtype=int)
+        del data_after_milling
+
+        histo, bins = np.histogram(img_after, bins=2000)
+        histo = np.cumsum(histo)
+        histo = histo / histo[-1]
+        plc = np.min(np.where(histo > 0.99))
+        thr_out_after = bins[plc] / 2 + bins[plc + 1] / 2
+        img_after[img_after > thr_out_after] = thr_out_after
+
+        if mean_zstack:
+            img_after = np.mean(img_after, axis=0)
+        else:
+            img_after = np.max(img_after, axis=0)
     else:
         img_after = np.array(data_after_milling[channel_after[nnn]], dtype=int)
+        del data_after_milling
 
-    del data_before_milling, data_after_milling
+        histo, bins = np.histogram(img_after, bins=2000)
+        histo = np.cumsum(histo)
+        histo = histo / histo[-1]
+        plc = np.min(np.where(histo > 0.99))
+        thr_out_after = bins[plc] / 2 + bins[plc + 1] / 2
+        img_after[img_after > thr_out_after] = thr_out_after
 
-    # setting a threshold for outliers in the image
-    histo, bins = np.histogram(img_before, bins=2000)
-    histo = np.cumsum(histo)
-    histo = histo / histo[-1]
-    plc = np.min(np.where(histo > 0.99))
-    thr_out_before = bins[plc] / 2 + bins[plc + 1] / 2
-    histo, bins = np.histogram(img_after.flatten(), bins=2000)
-    histo = np.cumsum(histo)
-    histo = histo / histo[-1]
-    plc = np.min(np.where(histo > 0.99))
-    thr_out_after = bins[plc] / 2 + bins[plc + 1] / 2
     del histo, bins
-    img_before[img_before > thr_out_before] = thr_out_before
-    img_after[img_after > thr_out_after] = thr_out_after
-
+    print("Pixel sizes are the same: {}".format(meta_before["Pixel size"][0] == meta_after["Pixel size"][0]))
     print("manual outlier threshold: \nbefore: {}\nafter: {}".format(manual_thr_out_before[nnn],
                                                                      manual_thr_out_after[nnn]))
     print("histogram 99% threshold: \nbefore: {}\nafter: {}".format(thr_out_before, thr_out_after))
@@ -119,7 +151,6 @@ for nnn in np.arange(0, 8, 1, dtype=int):
 
         else:
             img_rescaled = np.zeros(img_before.shape)
-            img_small = img_after
             d_pix = img_after.shape[0] / img_before.shape[
                 0]  # here we assume that the difference in x is the same as in
             # y as pixels are squares
@@ -199,16 +230,24 @@ for nnn in np.arange(0, 8, 1, dtype=int):
     img_after[img_after < 0] = 0
 
     # calculating the difference between the two images and creating a mask
-    diff = img_before_blurred - 2 * img_after_blurred  # times 2 to account for the intensity differences (more robust)
+    diff = img_before_blurred - 1.5*img_after_blurred  # times 2 to account for the intensity differences (more robust)
     mask = diff >= threshold_mask
-    mask2 = binary_opening(mask, iterations=int(blur / 2))  # First opening
+    mask2 = binary_opening(mask, iterations=int(2*blur/3))  # First opening
     # mask2 = binary_erosion(mask2, iterations=5)  # if the edges give too much signal we can erode the mask a bit more
-
-    masked_img = img_after * mask
-    masked_img2 = img_after * mask2
 
     # to crop the image around the ROI if possible and wanted: cropping = True
     index_mask = np.where(mask)
+    index_mask2 = np.where(mask2)
+
+    if squaring & (len(index_mask2[0]) != 0):
+        x_min = np.min(index_mask2[1])
+        y_min = np.min(index_mask2[0])
+        x_max = np.max(index_mask2[1])
+        y_max = np.max(index_mask2[0])
+        mask2[y_min:y_max, x_min:x_max] = True
+
+    masked_img = img_after * mask
+    masked_img2 = img_after * mask2
 
     if cropping & (len(index_mask[0]) != 0):
         x_min = np.min(index_mask[1])
@@ -225,8 +264,8 @@ for nnn in np.arange(0, 8, 1, dtype=int):
 
     # getting rid of too small ROIs and noise after the threshold
     # binary_end_result1 = binary_opening(binary_end_result1, iterations=5)
-    binary_end_result2 = binary_opening(binary_closing(binary_end_result2, iterations=1),
-                                        iterations=5)  # second opening
+    binary_end_result2 = binary_opening(binary_closing(binary_end_result2, iterations=2),
+                                        iterations=4)  # second opening
 
     # circles = HoughCircles(binary_end_result2, HOUGH_GRADIENT, 1, 20, param1=60, param2=20,
     #                        minRadius=0, maxRadius=img_after.shape[0] / 4)
@@ -271,5 +310,9 @@ for nnn in np.arange(0, 8, 1, dtype=int):
 
     ax[3, 1].set_title("end with 2nd opening")
     plt.show()
+
+    del img_before, img_before_blurred, img_after, img_after_blurred, diff, mask, mask2, masked_img, masked_img2, \
+        binary_end_result1, binary_end_result2, meta_before, meta_after, index_mask, index_mask2, fig, ax, \
+        x_min, x_max, y_min, y_max, shift, plc, dx_pix, dy_pix
 
     print("Successful!\n")
